23b4c68bc75e561078755fffa1d58686065cd85e
Nasibulin <k.nasibulin@yandex.ru>
2018-10-06 17:07:07 +0300

    -

---
+++ src/test/java/AutomataTest.java
@@ -0,0 +1,65 @@
+import org.junit.Test;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Konstantin
+ * Date: 06.10.18
+ * Time: 16:28
+ * To change this template use File | Settings | File Templates.
+ */
+public class AutomataTest {
+    @Test
+    public void testOn() throws Exception {
+
+    }
+
+    @Test
+    public void testOff() throws Exception {
+
+    }
+
+    @Test
+    public void testCoin() throws Exception {
+
+    }
+
+    @Test
+    public void testPrintMenu() throws Exception {
+
+    }
+
+    @Test
+    public void testPrintState() throws Exception {
+
+    }
+
+    @Test
+    public void testChoice() throws Exception {
+
+    }
+
+    @Test
+    public void testCheck() throws Exception {
+
+    }
+
+    @Test
+    public void testCancel() throws Exception {
+
+    }
+
+    @Test
+    public void testGetCash() throws Exception {
+
+    }
+
+    @Test
+    public void testCook() throws Exception {
+
+    }
+
+    @Test
+    public void testFinish() throws Exception {
+
+    }
+}
